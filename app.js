e1b87e0f13ee5cb4e40ad3eba9d1075a0bbf670e
Slavik <slavik.sukhanov@iskyweb.com>
2018-12-11 23:04:49 +0200

    feat: add morgan module, handling errors for no exist routes (404) and other errors (500)

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+app.use(morgan('dev'))
+
+
+// Routes
 const productsRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
-
 
 app.use('/products', productsRoutes)
 app.use('/orders', ordersRoutes)
 
+
+// Handling errors
+app.use((request, response, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, request, response, next) => {
+    response.status(error.status || 500)
+    response.json({
+        error: {
+            message: error.message,
+        }
+    })
+})
 
 module.exports = app
